8711b46473e9c6891285e0434ac8b6292ff22ad4
psncl <pushkarsinhaw@gmail.com>
2025-04-30 09:58:18 +0530

    Add exercise 12

---
+++ 12. bob.ts
@@ -0,0 +1,19 @@
+const isUpperCase = (sentence: string): boolean => [...sentence].every((c) => c.toUpperCase() === c);
+
+const containsLetters = (sentence: string): boolean => /[a-zA-Z]/.test(sentence);
+
+export function hey(message: string): string {
+    const trimmedMsg = message.trim();
+    switch (true) {
+        case (trimmedMsg === ''):
+            return 'Fine. Be that way!';
+        case (isUpperCase(trimmedMsg) && trimmedMsg.endsWith('?') && containsLetters(trimmedMsg)):
+            return 'Calm down, I know what I\'m doing!';
+        case (isUpperCase(trimmedMsg) && containsLetters(trimmedMsg)):
+            return 'Whoa, chill out!';
+        case (trimmedMsg.endsWith('?')):
+            return 'Sure.';
+        default:
+            return 'Whatever.';
+    }
+}
