5e60f62073a8644457f81be65a8dbb227c8ca4ee
psncl <pushkarsinhaw@gmail.com>
2025-04-30 16:41:17 +0530

    Add link to exercise 12

--- 12. bob.ts
+++ 12. bob.ts
@@ -1,19 +1,25 @@
-const isUpperCase = (sentence: string): boolean => [...sentence].every((c) => c.toUpperCase() === c);
+// https://exercism.org/tracks/typescript/exercises/bob
 
-const containsLetters = (sentence: string): boolean => /[a-zA-Z]/.test(sentence);
+const isUpperCase = (sentence: string): boolean =>
+  [...sentence].every((c) => c.toUpperCase() === c);
+
+const containsLetters = (sentence: string): boolean =>
+  /[a-zA-Z]/.test(sentence);
 
 export function hey(message: string): string {
-    const trimmedMsg = message.trim();
-    switch (true) {
-        case (trimmedMsg === ''):
-            return 'Fine. Be that way!';
-        case (isUpperCase(trimmedMsg) && trimmedMsg.endsWith('?') && containsLetters(trimmedMsg)):
-            return 'Calm down, I know what I\'m doing!';
-        case (isUpperCase(trimmedMsg) && containsLetters(trimmedMsg)):
-            return 'Whoa, chill out!';
-        case (trimmedMsg.endsWith('?')):
-            return 'Sure.';
-        default:
-            return 'Whatever.';
-    }
+  const trimmedMsg = message.trim();
+  switch (true) {
+    case trimmedMsg === "":
+      return "Fine. Be that way!";
+    case isUpperCase(trimmedMsg) &&
+      trimmedMsg.endsWith("?") &&
+      containsLetters(trimmedMsg):
+      return "Calm down, I know what I'm doing!";
+    case isUpperCase(trimmedMsg) && containsLetters(trimmedMsg):
+      return "Whoa, chill out!";
+    case trimmedMsg.endsWith("?"):
+      return "Sure.";
+    default:
+      return "Whatever.";
+  }
 }
